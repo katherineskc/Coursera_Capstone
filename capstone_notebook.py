c5a0974da1e307e8158124dfa18e33f688f8254b
Katherina Chen <katherineskc@gmail.com>
2020-01-27 12:29:41 +0000

    capstone notebook

---
+++ capstone_notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the juputer notebook for capstone project.
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
